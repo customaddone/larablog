d04eb5758e914a785352670440cba5c5c6fb374a
customfolk <custom1485@gmail.com>
2020-02-24 10:39:07 +0900

    alb #3

--- terraform/alb/aws_lb_listener.tf
+++ terraform/alb/aws_lb_listener.tf
@@ -1,24 +1,11 @@
+#どのポートのリクエストを受け付けるか
+
 # HTTPSでALBにアクセスできるようHTTPSリスナーを発行します
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.example.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "これは『HTTP』です"
-      status_code  = "200"
-    }
-  }
-}
-
 resource "aws_lb_listener" "https" {
   load_balancer_arn = aws_lb.example.arn
   port              = "443"
   protocol          = "HTTPS"
+  #先ほど発行したSSL証明書
   certificate_arn   = aws_acm_certificate.example.arn
   ssl_policy        = "ELBSecurityPolicy-2016-08"
 
@@ -29,6 +16,23 @@ resource "aws_lb_listener" "https" {
       content_type = "text/plain"
       message_body = "これは『HTTPS』です"
       status_code  = "200"
+    }
+  }
+}
+
+#リダイレクトします
+resource "aws_lb_listener" "redirect_http_to_https" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
     }
   }
 }
